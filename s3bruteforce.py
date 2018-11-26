578d4c355d1c15189b251cf76a00de56b454ebb5
Justin Gardner <justin@localhost.localdomain>
2018-11-26 07:25:25 -0500

    Adding tools

---
+++ s3bruteforce.py
@@ -0,0 +1,19 @@
+import requests
+import sys
+
+if len(sys.argv) < 3:
+	print "python s3bruteforce.py http://s3.amazonaws.com/{}.domain.com wordlist"
+	sys.exit(0)
+
+wordlist = open(sys.argv[2])
+data = wordlist.readlines()
+wordlist.close()
+
+for word in data:
+	word=word.strip()
+	req = requests.get(sys.argv[1].format(word))
+
+	if req.status_code!=404:
+		print " Found potential bucket: "+sys.argv[1].format(word)
+
+
